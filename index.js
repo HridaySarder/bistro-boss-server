abb7c2ee78bbeb0c531b0241a1a5e93d81db40ce
HridaySarder <hridoyaug12@gmail.com>
2023-11-16 13:02:03 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -38,6 +39,7 @@ app.get('/users', async(req,res) => {
 
 app.post('/users', async(req,res) => {
   const user = req.body;
+  console.log(user);
   const query = {email: user.email}
   const existingUser = await userCollection.findOne(query)
   if(existingUser){
@@ -45,6 +47,25 @@ app.post('/users', async(req,res) => {
   }
   const result = await userCollection.insertOne(user)
  res.send(result);
+})
+
+app.patch('/users/admin/:id', async(req,res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id)};
+  const updatedDoc = {
+    $set: {
+      role: 'admin'
+    }
+  }
+  const result = await userCollection.updateOne(filter,updatedDoc)
+  res.send(result)
+})
+
+app.delete('/users/:id', async (req,res) => {
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await userCollection.deleteOne(query)
+  res.send(result)
 })
 
 app.get('/menu', async(req,res) => {
